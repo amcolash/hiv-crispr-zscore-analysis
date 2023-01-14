a7dde1fe7960fbd9b88b4651abe817aab6a394a9
Vanessa M <montov@uw.edu>
2023-01-14 00:31:36 -0800

    Updated analysis script to output to individual folders

--- run_analysis.sh
+++ run_analysis.sh
@@ -27,49 +27,49 @@ function handle_error() {
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 pushd "$SCRIPT_DIR" > /dev/null
 
+# Make output directories if needed
+OUTDIR="${INPUT_FOLDER/input\//}"
+mkdir -p output/"$OUTDIR"/counts
+mkdir -p output/"$OUTDIR"/snakemake
+
 # Remove old count files and copy over new count files
 mkdir -p code/AM_count_combiner/input
 rm -f code/AM_count_combiner/input/*
 cp "$INPUT_FOLDER"/data/* code/AM_count_combiner/input
 
-if [[ $RUN_COMBINER == true ]]; then
   # Run the count combiner
   pushd code/AM_count_combiner > /dev/null
   echo Combining Count Files
   docker-compose up --build
   popd > /dev/null
-fi
 
-# Copy combined counts to snakemake locations
-mkdir -p output/counts
+# Make snakemake input dirs
 mkdir -p code/JT_snakemake/results/count
 mkdir -p code/JT_snakemake/results/extras
 mkdir -p code/JT_snakemake/results/test
 
-cp code/AM_count_combiner/counts_all.txt output/counts
+# Copy combined counts to snakemake locations
+cp code/AM_count_combiner/counts_all.txt output/"$OUTDIR"/counts
 cp code/AM_count_combiner/counts_all.txt code/JT_snakemake/results/count/
 
 # Copy configs, ignore errors
 cp "$INPUT_FOLDER/config/config_snakemake.yaml" code/JT_snakemake/config.yaml | true
 cp "$INPUT_FOLDER/config/config_combiner.js" code/AM_count_combiner/config.js | true
 
-# Make output directories if needed
-mkdir -p output/snakemake
-
-if [[ $RUN_SNAKEMAKE == true ]]; then
   # Run snakemake code
   pushd code/JT_snakemake > /dev/null
   echo Running Snakemake
 
+# Run Z-Score analysis
   ./run.sh -r
 
   # Copy snakemake output
   echo Copying Snakemake output files
-  cp results/extras/* ../../output/snakemake
-  cp results/test/* ../../output/snakemake
-  popd > /dev/null
-fi
+cp results/extras/* ../../output/"$OUTDIR"/snakemake
+cp results/test/* ../../output/"$OUTDIR"/snakemake
 
+# Move back to base dir
+  popd > /dev/null
 popd > /dev/null
 
 echo All Done!
